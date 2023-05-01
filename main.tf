60ca0379931743fe0a6f012bb88f982e4e3756a6
rulezz <rulezz@NMG-NB122.nmg.corp>
2023-05-01 20:52:59 +0300

    Security group defined using module

--- main.tf
+++ main.tf
@@ -22,11 +22,22 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "blog"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+  name    = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  egress_rules  = ["all-all"]
 }
 
 resource "aws_security_group" "blog" {
